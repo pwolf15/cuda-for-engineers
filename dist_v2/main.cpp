e1c6f535448a522dbbf79cb95045a159fba8b572
Patrick Wolf <pm_wolf@comcast.net>
2022-06-24 16:40:51 -0700

    Initial commit

---
+++ dist_v2/main.cpp
@@ -0,0 +1,28 @@
+#include "aux_functions.h"
+#include <stdlib.h>
+#include <iostream>
+
+#define N 20000000
+
+int main()
+{
+  float *in = (float*)calloc(N, sizeof(float));
+  float *out = (float*)calloc(N, sizeof(float));
+  const float ref = 0.5f;
+
+  for (int i = 0; i < N; ++i)
+  {
+    in[i] = scale(i, N);
+  }
+
+  distanceArray(out, in, ref, N);
+
+  for (int i = 0; i < 100; ++i)
+  {
+    std::cout << in[i] << "," << out[i] << std::endl;
+  }
+
+  free(in);
+  free(out);
+  return 0;
+}
